aa18c9532089f81bccd8ea1bd1e0aab19548fa0e
LimaGustav <lima.gustavo100@hotmail.com>
2022-05-08 16:08:59 -0300

    Aplicacao Windows Forms

---
+++ Aplicacao/Jogos/Jogos/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Jogos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(325, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "JOGOS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 57);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(776, 256);
+            this.dgv.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 377);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Aplicacao/Jogos/Jogos/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogos
+{
+    public partial class Form1 : Form
+    {
+        private ModuloDesktopEntities ctx = new ModuloDesktopEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.LightBlue;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void LoadData()
+        {
+            var jogos = ctx.Jogos.Select(x => new
+            {
+                Jogo = x,
+                Data = x.Data,
+                Hora = x.Data.Value.Hour,
+                Time1 = x.Selecao.Nome,
+                Placar1 = x.Placar1,
+                X = "X",
+                Placar2 = x.Placar2,
+                Time2 = x.Selecao3.Nome
+            }).ToList();
+
+            dgv.DataSource = jogos;
+
+            dgv.Columns["Jogo"].Visible = false;
+
+            for (int i = 0; i < dgv.Rows.Count; i++)
+            {
+                var jogo = dgv.Rows[i].Cells[0].Value as Jogos;
+
+                if (jogo.Placar1 > jogo.Placar2)
+                {
+                    dgv.Rows[i].Cells[3].Style.ForeColor = Color.Blue;
+                }
+
+                else if (jogo.Placar2 > jogo.Placar1)
+                    dgv.Rows[i].Cells[7].Style.ForeColor = Color.Blue;
+
+                else
+                {
+                    dgv.Rows[i].Cells[3].Style.BackColor = Color.Yellow;
+                    dgv.Rows[i].Cells[7].Style.BackColor = Color.Yellow;
+                }
+                   
+            }
+
+            //for (int i = 9; i <= 11; i++)
+            //    dgv.Columns[i].Visible = false;
+
+            //dgv.Columns["idjogo"].Visible = false;
+
+        }
+        
+    }
+}
